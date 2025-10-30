5d6e3dc098ab69e5f2de3c17d11e033176a79fab
🌟 **Leta  Kasahun** <letakasahun2@gmail.com>
2025-10-30 07:20:11 -0700

    Time: 16 ms (70.05%), Space: 12.4 MB (49.99%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,8 +4,11 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        num=str(x)
-        for i in range(len(num)):
-            num=[num::-1]
-        return int(num)
-
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        reversed_x = int(str(x)[::-1])
+        reversed_x *= sign
+        
+        if reversed_x < -2**31 or reversed_x > 2**31 - 1:
+            return 0
+        return reversed_x
