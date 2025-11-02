9380558d6046376827c0661378700b6b48a45818
🌟 **Leta  Kasahun** <letakasahun2@gmail.com>
2025-11-02 02:36:16 -0800

    Time: 13 ms (91.82%), Space: 13.4 MB (49.7%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        target=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if nums[i]+i>=target:
+                target=i
+        return target==0        
+        
