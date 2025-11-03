9c35fa3260a0b8dfd79347e06e2ee20c313c86e4
🌟 **Leta  Kasahun** <letakasahun2@gmail.com>
2025-11-03 02:28:25 -0800

    Time: 18 ms (62.78%), Space: 25.9 MB (70.48%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,10 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-      
-       filtered=set(nums)
-       if len(nums)==len(filtered):
-        return False
-       else:
-        return True 
+        filtered=set(nums) 
+        if len(nums)==len(filtered):
+            return False
+        else:
+            return True 
 
