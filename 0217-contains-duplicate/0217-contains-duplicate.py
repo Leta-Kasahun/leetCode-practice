f5f209e14cd37063ca849cdf9596e64d90bc2e41
🌟 **Leta  Kasahun** <letakasahun2@gmail.com>
2025-11-03 02:28:12 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+      
+       filtered=set(nums)
+       if len(nums)==len(filtered):
+        return False
+       else:
+        return True 
+
