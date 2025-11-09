a41e76eadbdfa5f1e7c0126f9d68bed1fa6f9932
🌟 **Leta  Kasahun** <letakasahun2@gmail.com>
2025-11-09 07:10:42 -0800

    Time: 1 ms (69.79%), Space: 12.4 MB (92.34%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
@@ -6,7 +6,6 @@ class Solution(object):
         """
         count=0
         for num in nums:
-            if num>=10:
-                if num%2==0:
+            if len(str(num))%2==0:
                     count+=1
         return count            
