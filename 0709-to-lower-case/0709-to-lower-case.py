d314ceafba69ec1e1d3765a135e5598b1d982e02
🌟 **Leta  Kasahun** <letakasahun2@gmail.com>
2025-11-17 07:33:10 -0800

    Time: 4 ms (2.34%), Space: 12.5 MB (52.76%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        lower_case=s.lower()
+        return lower_case
+        
