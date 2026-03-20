e0c17cf060ea10b3749285d9007888b328d06d0c
🌟 **Leta  Kasahun** <dbu1601306@dbu.edu.et>
2026-03-20 15:41:02 +0300

    Time: 273 ms (68.98%), Space: 78.3 MB (76.08%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,9 +1,9 @@
-# class Node:
-#     def __init__(self, key, value):
-#         self.key = key
-#         self.value = value
-#         self.prev = None
-#         self.next = None
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
 
 class LRUCache(object):
