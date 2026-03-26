e55cac31aaa6d696441d8ae2e1d4d55d19b22247
🌟 **Leta  Kasahun** <dbu1601306@dbu.edu.et>
2026-03-26 09:02:52 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -2,12 +2,10 @@ class Solution(object):
     def countPrimes(self, n):
         if n <= 2:
             return 0
-
         # Step 1: assume all numbers are prime
         isPrime = [True] * n
+        # 0 and 1 are not prime
         isPrime[0] = isPrime[1] = False 
-         # 0 and 1 are not prime
-
         # Step 2: remove multiples
         for i in range(2, int(n**0.5) + 1):
             if isPrime[i]:
