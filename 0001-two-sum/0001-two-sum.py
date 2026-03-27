67cf5c3c5f4ba2c88756f8436ca4654b46016cf7
🌟 **Leta  Kasahun** <dbu1601306@dbu.edu.et>
2026-03-27 07:38:45 +0300

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+       
         num_to_index = {}
         for i, num in enumerate(nums):
             complement = target - num
