0d35f79b99ff055e56bddc5ad615c46681d66010
🌟 **Leta  Kasahun** <dbu1601306@dbu.edu.et>
2026-04-30 22:01:10 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,24 @@
 class Solution(object):
     def twoSum(self, nums, target):
-       
-        num_to_index = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-            num_to_index[num] = i
+        """
+        # :type nums: List[int]
+        # :type target: int
+        # :rtype: List[int]
+        # """
+        # #  this  can   be solved in different  ways  using  hash map  with  two poinstes 
+        # # using  nexted loops   O(n2)  and 
+        # for i  in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #        if nums[i]+nums[j]==target:
+        #            return [i,j]
+        # return None    
+
+
+        #it  can  be also  solved with  order of   O(n) using hash map
+        seen={}
+        for i in range(len(nums)):
+            diff=target-nums[i]
+            if  diff in  seen:
+                return [seen[diff],i]
+            seen[nums[i]]=i
+        return None
